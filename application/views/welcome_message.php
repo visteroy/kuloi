9b3b51d7101bc708fa6bd5671544186bf1664b98
Administrator <Administrator@May18>
2013-11-20 16:30:52 +0700

    asflasdkfls

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,84 +1,108 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to Ku Loi Homepage</title>
-        <link href="<?php echo base_url();?>css/bootstrap.css" rel="stylesheet">
-        <script src="<?php echo base_url();?>js/bootstrap.js"></script>
-	<style type="text/css">
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome to Ku Loi Homepage</title>
+        <link href="<?php echo base_url(); ?>css/bootstrap.css" rel="stylesheet">
+        <script src="<?php echo base_url(); ?>js/bootstrap.js"></script>
+        <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+        <style type="text/css">
 
-	::selection{ background-color: #E13300; color: white; }
-	::moz-selection{ background-color: #E13300; color: white; }
-	::webkit-selection{ background-color: #E13300; color: white; }
+            ::selection{ background-color: #E13300; color: white; }
+            ::moz-selection{ background-color: #E13300; color: white; }
+            ::webkit-selection{ background-color: #E13300; color: white; }
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
+            body {
+                background-color: #fff;
+                margin: 40px;
+                font: 13px/20px normal Helvetica, Arial, sans-serif;
+                color: #4F5155;
+            }
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
+            a {
+                color: #003399;
+                background-color: transparent;
+                font-weight: normal;
+            }
 
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
+            h1 {
+                color: #444;
+                background-color: transparent;
+                border-bottom: 1px solid #D0D0D0;
+                font-size: 19px;
+                font-weight: normal;
+                margin: 0 0 14px 0;
+                padding: 14px 15px 10px 15px;
+            }
 
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
+            code {
+                font-family: Consolas, Monaco, Courier New, Courier, monospace;
+                font-size: 12px;
+                background-color: #f9f9f9;
+                border: 1px solid #D0D0D0;
+                color: #002166;
+                display: block;
+                margin: 14px 0 14px 0;
+                padding: 12px 10px 12px 10px;
+            }
 
-	#body{
-		margin: 0 15px 0 15px;
-	}
-	
-	p.footer{
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-	
-	#container{
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		-webkit-box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
-<body>
+            #body{
+                margin: 0 15px 0 15px;
+            }
 
-<div id="container">
-	<h1>Welcome to Ku Loi Homepage!</h1>
+            p.footer{
+                text-align: right;
+                font-size: 11px;
+                border-top: 1px solid #D0D0D0;
+                line-height: 32px;
+                padding: 0 10px 0 10px;
+                margin: 20px 0 0 0;
+            }
 
-	<div id="body">
-		
-		<code>Document of Mr Vu will appear here.</code>
+            #container{
+                margin: 10px;
+                border: 1px solid #D0D0D0;
+                -webkit-box-shadow: 0 0 8px #D0D0D0;
+            }
+            .topmenu {
+                padding: 10px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="topmenu">
+            <ul class="nav nav-pills">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Messages</a></li>
+                <li style="float: right;">
+                    <form class="form-inline" role="form" method=post action="<?php echo base_url(); ?>user/check_login">
+                        <div class="form-group">
+                            <label class="sr-only" for="exampleInputEmail1">Username</label>
+                            <input type=text name=username class="form-control" id="exampleInputEmail1" placeholder="Enter username">
+                        </div>
+                        <div class="form-group">
+                            <label class="sr-only" for="exampleInputPassword1">Password</label>
+                            <input type=password name=password class="form-control" id="exampleInputPassword1" placeholder="Enter password">
+                        </div>
+                        <input type="submit" name="submit_btn" class="btn btn-primary" value="Login">
+                        <input type=reset class="btn btn-danger" value="Reset">         
+                    </form>
+                </li>
+            </ul>            
+        </div>
+        <div id="container">            
+            <h1>Welcome to Ku Loi Homepage!</h1>
 
-		<a class="btn btn-success"  href="<?php echo base_url();?>user">Login here!</a>
-	</div>
+            <div id="body">
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
-</div>
+                <code>Document of Mr Vu will appear here.</code>
 
-</body>
+                <a class="btn btn-success"  href="<?php echo base_url(); ?>user">Login here!</a>
+            </div>
+
+            <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+        </div>
+
+    </body>
 </html>
